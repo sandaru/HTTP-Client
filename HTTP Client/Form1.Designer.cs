43ebbd6d84c9936dd5a7f62e15e617507481ee65
Pushpitha Sandaruwan <sandaru.weerasooriya@gmail.com>
2014-05-08 16:01:55 +0530

    C# HTTP Client
    HTTP Client application to communicate with python server

---
+++ HTTP Client/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace HTTP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textNatType = new System.Windows.Forms.TextBox();
+            this.textPort = new System.Windows.Forms.TextBox();
+            this.textPublicIP = new System.Windows.Forms.TextBox();
+            this.richTextBoxRespose = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textMachineName = new System.Windows.Forms.TextBox();
+            this.textDataString = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textNatType
+            // 
+            this.textNatType.BackColor = System.Drawing.Color.White;
+            this.textNatType.Location = new System.Drawing.Point(126, 58);
+            this.textNatType.Name = "textNatType";
+            this.textNatType.ReadOnly = true;
+            this.textNatType.Size = new System.Drawing.Size(166, 20);
+            this.textNatType.TabIndex = 0;
+            // 
+            // textPort
+            // 
+            this.textPort.BackColor = System.Drawing.Color.White;
+            this.textPort.Location = new System.Drawing.Point(126, 132);
+            this.textPort.Name = "textPort";
+            this.textPort.ReadOnly = true;
+            this.textPort.Size = new System.Drawing.Size(166, 20);
+            this.textPort.TabIndex = 1;
+            // 
+            // textPublicIP
+            // 
+            this.textPublicIP.BackColor = System.Drawing.Color.White;
+            this.textPublicIP.Location = new System.Drawing.Point(126, 94);
+            this.textPublicIP.Name = "textPublicIP";
+            this.textPublicIP.ReadOnly = true;
+            this.textPublicIP.Size = new System.Drawing.Size(166, 20);
+            this.textPublicIP.TabIndex = 2;
+            // 
+            // richTextBoxRespose
+            // 
+            this.richTextBoxRespose.Location = new System.Drawing.Point(13, 209);
+            this.richTextBoxRespose.Name = "richTextBoxRespose";
+            this.richTextBoxRespose.Size = new System.Drawing.Size(279, 84);
+            this.richTextBoxRespose.TabIndex = 3;
+            this.richTextBoxRespose.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Send Request";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NAT Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Public IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "PORT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Machine Name";
+            // 
+            // textMachineName
+            // 
+            this.textMachineName.BackColor = System.Drawing.Color.White;
+            this.textMachineName.Location = new System.Drawing.Point(126, 21);
+            this.textMachineName.Name = "textMachineName";
+            this.textMachineName.ReadOnly = true;
+            this.textMachineName.Size = new System.Drawing.Size(166, 20);
+            this.textMachineName.TabIndex = 8;
+            // 
+            // textDataString
+            // 
+            this.textDataString.BackColor = System.Drawing.Color.White;
+            this.textDataString.Location = new System.Drawing.Point(15, 183);
+            this.textDataString.Name = "textDataString";
+            this.textDataString.ReadOnly = true;
+            this.textDataString.Size = new System.Drawing.Size(277, 20);
+            this.textDataString.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Data String";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 338);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textDataString);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textMachineName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBoxRespose);
+            this.Controls.Add(this.textPublicIP);
+            this.Controls.Add(this.textPort);
+            this.Controls.Add(this.textNatType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textNatType;
+        private System.Windows.Forms.TextBox textPort;
+        private System.Windows.Forms.TextBox textPublicIP;
+        private System.Windows.Forms.RichTextBox richTextBoxRespose;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textMachineName;
+        private System.Windows.Forms.TextBox textDataString;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HTTP Client/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumiSoft.Net.STUN.Client;
+using System.Net.Sockets;
+using System.Collections.Specialized;
+
+namespace HTTP_Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //getResponse();
+            //sendRequest(null);
+            applicationOn();
+        }
+
+        /// <summary>
+        /// iterative method to get response
+        /// this is listning for the server state related to the client
+        /// </summary>
+        /// <param name="value"></param>
+
+        public void getResponse()
+        {
+            //timer1.Start();
+        }
+
+        /// <summary>
+        /// Method to send request with the PORT and public IP
+        /// Run when application is loading.
+        /// </summary>
+        /// <param name="value"></param>
+        public void sendRequest(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string> (sendRequest), new object[] {value});
+                return;
+            }
+            String[] stundata = getStundata();
+            textNatType.Text = stundata[0];//NAt type
+            textPort.Text = stundata[1];//Public Port
+            textPublicIP.Text = stundata[2];//Public IP
+            textMachineName.Text = System.Environment.MachineName;//Computer name
+            textDataString.Text = stundata[0] + ":" + stundata[1] + ":" + stundata[2] + ":" + System.Environment.MachineName;
+        }
+
+        /// <summary>
+        /// Connect to a stun server to get the public IP and PORT
+        /// </summary>
+        /// <returns></returns>
+        private string[] getStundata()
+        {
+            string[] stundata = new String[3];
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+            STUN_Result result = STUN_Client.Query("stunserver.org", 3478, socket);
+            stundata[0] = result.NetType.ToString();
+            if (result.NetType != STUN_NetType.UdpBlocked)
+            {
+                String[] temp = result.PublicEndPoint.ToString().Split(':');
+                stundata[1] = temp[1];
+                stundata[2] = temp[0];
+            }
+            else
+            {
+                MessageBox.Show("Error Public IP");
+            }
+            return stundata;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    try
+                    {
+                        var response = client.DownloadString("http://localhost:8787/");
+                        richTextBoxRespose.Text += response + Environment.NewLine;
+                    }
+                    catch (ArgumentException)//if url is empty
+                    {
+                        MessageBox.Show("Download String is null or wrong");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                timer1.Stop();
+                MessageBox.Show("Error in Request sending procedure");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //sendRequest(null);
+           // applicationOn();
+        }
+
+        private void applicationOn()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    var values = new NameValueCollection();
+                    values["port"] = "hello";
+
+                    var ret = client.UploadValues("http://localhost:8787/postval", values);
+                    MessageBox.Show(Encoding.ASCII.GetString(ret));
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+        }
+
+
+    }
+}
